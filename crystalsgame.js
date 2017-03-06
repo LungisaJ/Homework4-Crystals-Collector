0c7fbad2064f33efcd1d6246c3e3b69d660ec10b
Lungisa <lungisajohnson@gmail.com>
2017-03-06 17:46:12 -0500

    Add files via upload

---
+++ crystalsgame.js
@@ -0,0 +1,28 @@
+$(document).ready(funtion()){
+	$("#button1").click(function()){
+	document.getElementById().innerHTML=button1;
+	button1=Math.random(0,12);
+
+	}
+}
+
+button1=Math.floor(Math.random(0,12));
+button2=Math.floor(Math.random(0,12));
+button3=Math.floor(Math.random(0,12));
+button4=Math.floor(Math.random(0,12));
+document.getElementById("button1")innerHTML=number1;
+
+
+  //document.getElementById().innerHTML=button1;
+
+
+   //$ ("#button").click(function)(){
+	//function getRandom() {
+  	//return Math.random();
+
+  //  Click events are done for us:
+  var rand = Math.floor(Math.random(0,12));
+	//$("#button1").button1click(rand);
+	//$("#button2").click(rand);
+	//$("#button3").click(rand);
+	//$("#button4").click(rand);
